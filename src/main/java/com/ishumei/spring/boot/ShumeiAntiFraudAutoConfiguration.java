95733f161699782677a17ae7bf5c2916b75e4fb8
wandl <hiwepy@163.com>
2021-04-20 20:51:20 +0800

    优化

--- src/main/java/com/ishumei/spring/boot/ShumeiAntiFraudAutoConfiguration.java
+++ src/main/java/com/ishumei/spring/boot/ShumeiAntiFraudAutoConfiguration.java
@@ -2,7 +2,7 @@ package com.ishumei.spring.boot;
 
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,32 +15,39 @@ import okhttp3.OkHttpClient;
 
 
 @Configuration
-@EnableConfigurationProperties(ShumeiAntiFraudProperties.class)
+@EnableConfigurationProperties({ShumeiAntiFraudProperties.class, ShumeiOkHttp3Properties.class})
 public class ShumeiAntiFraudAutoConfiguration {
-
-	@Bean
-	@ConditionalOnMissingBean
-	public OkHttpClient okhttp3Client() {
-		OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        builder.connectTimeout(6L, TimeUnit.SECONDS);
-        builder.readTimeout(6L, TimeUnit.SECONDS);
-        builder.writeTimeout(6L, TimeUnit.SECONDS);
-        ConnectionPool connectionPool = new ConnectionPool(50, 60, TimeUnit.SECONDS);
-        builder.connectionPool(connectionPool);
-        return builder.build();
-	}
-	
-	@Bean
-	@ConditionalOnMissingBean
-	public ObjectMapper objectMapper() {
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		return objectMapper;
-	}
 	
 	@Bean
 	public ShumeiAntiFraudTemplate shumeiAntiFraudTemplate(ShumeiAntiFraudProperties properties,  
-			ObjectMapper objectMapper, OkHttpClient okhttp3Client) {
+			ShumeiOkHttp3Properties okHttp3Properties,
+			ObjectProvider<ObjectMapper> objectMapperProvider,	
+			ObjectProvider<OkHttpClient> okhttp3ClientProvider) {
+		
+		ObjectMapper objectMapper = objectMapperProvider.getIfAvailable(() -> { 
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+			return mapper;
+		});
+		
+		OkHttpClient okhttp3Client = okhttp3ClientProvider.getIfAvailable(() -> { 
+			
+			OkHttpClient.Builder builder = new OkHttpClient.Builder()
+				.callTimeout(okHttp3Properties.getCallTimeout(), TimeUnit.SECONDS)
+				.connectionPool(new ConnectionPool(okHttp3Properties.getMaxIdleConnections(),
+						okHttp3Properties.getKeepAliveDuration().getSeconds(), TimeUnit.SECONDS))
+				.connectTimeout(okHttp3Properties.getConnectTimeout(), TimeUnit.SECONDS)
+				.followRedirects(okHttp3Properties.isFollowRedirects())
+				.followSslRedirects(okHttp3Properties.isFollowSslRedirects())
+				.pingInterval(okHttp3Properties.getPingInterval(), TimeUnit.SECONDS)
+				.readTimeout(okHttp3Properties.getReadTimeout(), TimeUnit.SECONDS)
+				.retryOnConnectionFailure(okHttp3Properties.isRetryOnConnectionFailure())
+				.writeTimeout(okHttp3Properties.getWriteTimeout(), TimeUnit.SECONDS);
+				
+	        return builder.build();
+	        
+		});
+		
 		return new ShumeiAntiFraudTemplate(properties, objectMapper, okhttp3Client);
 	}
 	
