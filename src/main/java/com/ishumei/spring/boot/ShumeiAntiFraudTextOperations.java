94c41a2a10165a16e66e934f2c56c633f7edf121
wandl <hiwepy@163.com>
2021-11-22 14:50:10 +0800

    优化

--- src/main/java/com/ishumei/spring/boot/ShumeiAntiFraudTextOperations.java
+++ src/main/java/com/ishumei/spring/boot/ShumeiAntiFraudTextOperations.java
@@ -22,7 +22,7 @@ import com.ishumei.spring.boot.model.AntiFraudTextRequestData;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- *     智能文本识别
+ * 智能文本识别
  * https://www.ishumei.com/help/documents.html?id=21110
  */
 @Slf4j
@@ -38,12 +38,12 @@ public class ShumeiAntiFraudTextOperations extends ShumeiAntiFraudOperations {
 	 * @param type  平台业务类型，可选值(必须大写)；直播：ZHIBO、电商：ECOM、游戏：GAME、新闻资讯 ：NEWS、论坛：FORUM、社交 ：SOCIAL
 	 * @param tokenId 客户端用户唯一标识；用于用户行为分析，建议传入用户UID；注：不同用户务必传入不同的tokenId对其进行唯一标识
 	 * @param nickname 用户昵称；强烈建议传递此参数，几乎所有平台的恶意用户都会通过昵称散播垃圾信息，存在涉政违禁和导流信息等风险
-	 * @return 
+	 * @return
 	 */
 	public AntiFraudResponse antiFraud(String type, String tokenId, String nickname) {
 		return this.antiFraud(type, tokenId, nickname, nickname);
 	}
-	
+
 	/**
 	 * 2、智能文本识别
 	 * API：https://www.ishumei.com/help/documents.html?id=21110
@@ -51,28 +51,28 @@ public class ShumeiAntiFraudTextOperations extends ShumeiAntiFraudOperations {
 	 * @param tokenId 客户端用户唯一标识；用于用户行为分析，建议传入用户UID；注：不同用户务必传入不同的tokenId对其进行唯一标识
 	 * @param nickname 用户昵称；强烈建议传递此参数，几乎所有平台的恶意用户都会通过昵称散播垃圾信息，存在涉政违禁和导流信息等风险
 	 * @param text 要检测的文本内容；文本上限2W字
-	 * @return 
+	 * @return
 	 */
 	public AntiFraudResponse antiFraud(String type, String tokenId, String nickname, String text) {
-		
+
 		AntiFraudTextRequest payload = new AntiFraudTextRequest();
 		payload.setAccessKey(getTemplate().getProperties().getAccessKey());
 		payload.setAppId(getTemplate().getProperties().getAppId());
 		payload.setType(type);
-		
+
 		AntiFraudTextRequestData data = new AntiFraudTextRequestData();
 		data.setChannel(getTemplate().getProperties().getChannelTxt());
 		data.setTokenId(tokenId);
 		data.setText(text);
 		data.setNickname(nickname);
-		
+
 		payload.setData(data);
-        
+
 		AntiFraudResponse res = request(getTemplate().getProperties().getAntiFraudTxtUrl(), payload, AntiFraudResponse.class);
 		if (!res.isSuccess()) {
 			log.error("文本识别失败：code: {}、RequestId: {}、Message: {}", res.getCode(), res.getRequestId(), res.getMessage());
 		}
 		return res;
 	}
-	
+
 }
